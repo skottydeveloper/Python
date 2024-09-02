fc6df3c859a8cd9a21b681cf9b35e57cce8a4eb5
Andreas Skotadis <skottydeveloper@gmail.com>
2024-09-02 20:27:14 +1000

    Fix error that caused incorrect calculations.

--- exercises/LovelyLoveseats.py
+++ exercises/LovelyLoveseats.py
@@ -26,7 +26,7 @@ customer_one_itemization += luxurious_lamp_description
 
 customer_one_tax = customer_one_total * sales_tax
 
-customer_one_total += sales_tax
+customer_one_total += customer_one_tax
 
 print("Customer One Items:")
 
